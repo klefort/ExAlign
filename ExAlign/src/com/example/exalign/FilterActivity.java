7d831c5c3560d6b0ed39aa4af6a67fab17fcc38c
jamespar147 <jamespar@bu.edu>
2014-11-06 20:28:32 -0500

    a

---
+++ ExAlign/src/com/example/exalign/FilterActivity.java
@@ -0,0 +1,18 @@
+package com.example.exalign;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class FilterActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.camera_activity);
+	}
+}
